5346cd5c1bc4be2c2e409c1cdea7f7aaad513871
AshharShah <ashhar.shah786@gmail.com>
2022-07-28 12:27:14 +0500

    Completed the game

--- Guess The Number/script.py
+++ Guess The Number/script.py
@@ -1,24 +1,29 @@
 import random as rand
+import os
 print('Welcome to Number Guessing Game!\n')
 print('Rules: ')
 print('1) The number to be guessed is between 1 and 25')
 print('2) You have 5 tries to guess the number \n')
 
+again = 1
+while(again == 1):
+    num = rand.randint(1, 25)
+    tries = 5
+    print(num)
 
-num = rand.randint(1, 25)
-tries = 5
-print(num)
+    while(tries != 0):
+        guess = int(input('Enter your guess: '))
+        if(guess > num):
+            print('Go Lesser')
+        elif(guess < num):
+            print('Go Higher')
+        else:
+            print('Well Done! You Guessed The Number')
+            break
+        tries = tries - 1
 
-while(tries != 0):
-    guess = int(input('Enter your guess: '))
-    if(guess > num):
-        print('Go Lesser')
-    elif(guess < num):
-        print('Go Higher')
-    else:
-        print('Well Done! You Guessed The Number')
-        break
-    tries = tries - 1
-
-if(tries == 0):
-    print('You Lost! The Number Was:', num)
+    if(tries == 0):
+        print('You Lost! The Number Was:', num)
+    again = int(input('Enter 1 to play again! '))
+    if(again == 1):
+        os.system('CLS')
