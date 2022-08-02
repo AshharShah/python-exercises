73d816ee6b411e541dfe0c77c8d0e829ae929ffc
AshharShah <ashhar.shah786@gmail.com>
2022-08-02 11:10:59 +0500

    Script Finalized

---
+++ Health Management System/README.md
@@ -0,0 +1,3 @@
+# Health Management System
+
+We give the user 3 options for which he has to input data, he can either log and exercise or a diet. Once we have all the users input we open the cooresponding file and then append a new row to the end of the file.
--- Health Management System/script.py
+++ Health Management System/script.py
@@ -1,5 +1,6 @@
 import csv
 import os
+from csv import writer
 
 
 def getdate():
@@ -22,6 +23,16 @@ def getType():
     return int(input("Enter the corresponding number: "))
 
 
+def append_list_as_row(file_name, list_of_elem):
+    # Open file in append mode
+    with open(file_name, 'a+', newline='') as write_obj:
+        # Create a writer object from csv module
+        csv_writer = writer(write_obj)
+        # Add contents of list as last row in the csv file
+        csv_writer.writerow(list_of_elem)
+        write_obj.close()
+
+
 logType = getType()
 print("\n")
 logName = getName()
@@ -29,21 +40,32 @@ global taskName
 
 if (logType == 1):
     if(logName == 1):
-        file = open('Health Management System\harry_diet.csv')
         taskName = input("What did Harry Eat?\n")
+        newInfo = [getdate(), taskName]
+        append_list_as_row('Health Management System\harry_diet.csv', newInfo)
     elif(logName == 2):
-        file = open('Health Management System\\ashhar_diet.csv')
         taskName = input("What did Ashhar Eat?\n")
+        newInfo = [getdate(), taskName]
+        append_list_as_row(
+            'Health Management System\\ashhar_diet.csv', newInfo)
+        print(newInfo)
     elif(logName == 3):
-        file = open('Health Management System\\bilal_diet.csv')
         taskName = input("What did Bilal Eat?\n")
+        newInfo = [getdate(), taskName]
+        append_list_as_row('Health Management System\\bilal_diet.csv', newInfo)
 elif (logType == 2):
     if(logName == 1):
-        file = open('Health Management System\harry_exercise.csv')
         taskName = input("What exercise did Harry do?\n")
+        append_list_as_row(
+            'Health Management System\harry_exercise.csv', newInfo)
+        newInfo = [getdate(), taskName]
     elif(logName == 2):
-        file = open('Health Management System\\ashhar_exercise.csv')
         taskName = input("What exercise did Ashhar do?\n")
+        newInfo = [getdate(), taskName]
+        append_list_as_row(
+            'Health Management System\\ashhar_exercise.csv', newInfo)
     elif(logName == 3):
-        file = open('Health Management System\\bilal_exercise.csv')
         taskName = input("What exercise did Bilal do?\n")
+        newInfo = [getdate(), taskName]
+        append_list_as_row(
+            'Health Management System\\bilal_exercise.csv', newInfo)
